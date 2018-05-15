09a7b846426f755b8555f7d3fd2a208da22be484
William Wu <williamwu0013@gmail.com>
2018-05-14 20:21:50 -0400

    Restructured Player class into Character class. Added subclasses for Hero and Player objects.

---
+++ Assets/Scripts/CharacterClass.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterClass : MonoBehaviour {
+	public static readonly int BASE_SPEED = 10;
+	public static int nodeID {get; private set;}
+	public static float speed {get; private set;}
+	public static Transform trans;
+	public static Rigidbody2D rb;
+	public static SpriteRenderer rend;
+	public static Collider2D iColl;
+	public static void setNodeID(int id){nodeID = id;}
+
+	void Awake()
+	{
+		trans = GetComponent<Transform>();
+		rb = GetComponent<Rigidbody2D>();
+		rend = GetComponent<SpriteRenderer>();
+		iColl = GameObject.Find("InteractionCollider").GetComponent<Collider2D>();
+	}
+
+	void Start () {
+		reset();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public static void reset() {
+		speed = BASE_SPEED;
+	}
+}
---
+++ Assets/Scripts/PlayerClass.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//ROLE: controls player status
+
+public class PlayerClass : CharacterClass
+{
+	public static readonly int DAILY_FOOD_REQUIREMENT = 30;
+
+	public static bool isHuman {get; private set;}
+	public static bool isFed {get; private set;}
+	private int food;
+
+	// Use this for initialization
+	void Start()
+	{
+		CharacterClass.reset();
+		isHuman = true;
+		isFed = false;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		//check to see if player is clicking at exit to go to next worldnode
+
+	}
+
+	public void toggleDayNight()
+	{
+		if(GameState.isDaytime)
+		{
+			isHuman = true;
+			isFed = false;
+		}
+		else
+		{
+			isHuman = false;
+		}
+	}
+
+	//will also be used when replaying game
+	//place all initializations in here
+
+	private void addFood(int value)
+	{
+		food += value;
+		if(food >= DAILY_FOOD_REQUIREMENT)
+			isFed = true;
+	}
+}
