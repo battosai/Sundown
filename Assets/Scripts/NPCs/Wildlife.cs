66a87eb3b56c7416e29c8027c70bceaa07656f27
Brian Tsai <bt92130@gmail.com>
2018-06-09 00:04:53 -0700

    added wildlife npc script

---
+++ Assets/Scripts/NPCs/Wildlife.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Wildlife : MonoBehaviour
+{
+    public int health {get; private set;}
+    public float speed {get; private set;}
+    public List<List<GameObject>> wildlife {get; private set;}
+    public Rigidbody2D rb {get; private set;}
+
+    public void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void Update()
+    {
+        idleWalk();
+    }
+
+    private void idleWalk()
+    {
+        int action = Random.Range(0, 1);
+        if(action == 0)
+            rb.velocity = Vector2.zero;
+        else
+        {
+            float horizontal = Random.Range(-speed, speed);
+            float vertical = Random.Range(-speed, speed);
+            rb.velocity = new Vector2(horizontal, vertical);
+        }
+    } 
+}
